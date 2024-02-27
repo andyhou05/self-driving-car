2737d9863a0fb513221ef4691a970fe229f1de56
andyhou05 <131478635+andyhou05@users.noreply.github.com>
2024-02-27 12:38:27 -0500

    Add files via upload

---
+++ javafx-template-main/src/main/java/edu/vanier/template/MainApp.java
@@ -0,0 +1,50 @@
+package edu.vanier.template;
+
+import edu.vanier.template.controllers.FXMLMainAppController;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a JavaFX project template to be used for creating GUI applications.
+ * The JavaFX GUI framework (version: 20.0.2) is linked to this project in the
+ * build.gradle file.
+ * @link: https://openjfx.io/javadoc/20/
+ * @see: /Build Scripts/build.gradle
+ * @author Sleiman Rabah.
+ */
+public class MainApp extends Application {
+
+    private final static Logger logger = LoggerFactory.getLogger(MainApp.class);
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            logger.info("Bootstrapping the application...");
+            //-- 1) Load the scene graph from the specified FXML file and 
+            // associate it with its FXML controller.
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainApp_layout.fxml"));
+            loader.setController(new FXMLMainAppController());
+            Pane root = loader.load();
+            //-- 2) Create and set the scene to the stage.
+            Scene scene = new Scene(root, 500, 300);
+            primaryStage.setScene(scene);
+            primaryStage.sizeToScene();
+            // We just need to bring the main window to front.
+            primaryStage.setAlwaysOnTop(true);            
+            primaryStage.show();
+            primaryStage.setAlwaysOnTop(false);
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
