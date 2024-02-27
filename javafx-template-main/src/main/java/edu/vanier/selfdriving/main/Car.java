39d1154d5202f71775b9b4ea6356abed88ed1793
Lu Yun Chen <2273410@edu.vaniercollege.qc.ca>
2024-02-27 13:07:55 -0500

    Initiated Car class

---
+++ javafx-template-main/src/main/java/edu/vanier/selfdriving/main/Car.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.selfdriving.main;
+
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author 2273410
+ */
+public class Car {
+    //Car image
+    Image carImage;
+    
+    //Static properties
+    double mass;
+    
+    //Transition properties
+    double speedX;
+    double speedY;
+    double rotationAngle;
+    
+    
+    
+}
