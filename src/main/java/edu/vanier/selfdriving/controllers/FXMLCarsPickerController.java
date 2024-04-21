0c247a062dd6e6093a305414412fd1ec7165d880
Mustard Gaming <luyunchen04@gmail.com>
2024-04-21 00:48:13 -0400

    Added car picker

--- src/main/java/edu/vanier/selfdriving/controllers/FXMLCarsPickerController.java
+++ src/main/java/edu/vanier/selfdriving/controllers/FXMLCarsPickerController.java
@@ -1,7 +1,11 @@
 package edu.vanier.selfdriving.controllers;
 
-import edu.vanier.selfdriving.models.Car;
+import edu.vanier.selfdriving.main.Main;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -18,6 +22,14 @@ import javafx.scene.image.ImageView;
 public class FXMLCarsPickerController {
     public static int typeCounter = 0;
     @FXML
+    Button car1;
+    @FXML
+    Button car2;
+    @FXML
+    Button car3;
+    @FXML
+    Button car4;
+    @FXML
     Button returnButton;
     @FXML
     ImageView firstImage;
@@ -29,23 +41,51 @@ public class FXMLCarsPickerController {
     ImageView forthImage;
     @FXML
     Button switchCars;
+
     
-    
+    public static String[] listOfColors = {"black","blue","green","red"};
+    public static int[] listOfTypes = {1,2,3,4};
     public void updateCars(){
-        
+        firstImage.setImage(new Image("/sprites/car_"+listOfColors[0]+"_"+listOfTypes[typeCounter%4]+".png"));
+        secondImage.setImage(new Image("/sprites/car_"+listOfColors[1]+"_"+listOfTypes[typeCounter%4]+".png"));
+        thirdImage.setImage(new Image("/sprites/car_"+listOfColors[2]+"_"+listOfTypes[typeCounter%4]+".png"));
+        forthImage.setImage(new Image("/sprites/car_"+listOfColors[3]+"_"+listOfTypes[typeCounter%4]+".png"));
+        FXMLGameController.carNumber = Integer.toString(listOfTypes[typeCounter%4]);
     }
-    
+
     
     @FXML
-    void initalize(){
-        
-        
+    public void initialize(){
+            
+        FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/fxml/gamemode.fxml"));
+         FXMLGamemodeController menuController = new FXMLGamemodeController(Main.scene);
+         menuLoader.setController(menuController);
+        updateCars();
         
         switchCars.setOnAction(e ->{
             typeCounter+=1;
             updateCars();
-    });
         
+    });
+        returnButton.setOnAction(e->{
+            try {
+                Main.scene.setRoot(menuLoader.load());
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLGamemodeController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        car1.setOnAction(e->{
+            FXMLGameController.carColor = "black";
+        });
+        car2.setOnAction(e->{
+            FXMLGameController.carColor = "blue";
+        });
+        car3.setOnAction(e->{
+            FXMLGameController.carColor = "green";
+        });
+        car4.setOnAction(e->{
+            FXMLGameController.carColor = "red";
+        });
         
     }
 }
--- src/main/java/edu/vanier/selfdriving/controllers/FXMLGameController.java
+++ src/main/java/edu/vanier/selfdriving/controllers/FXMLGameController.java
@@ -36,10 +36,10 @@ public class FXMLGameController {
     CarController carController;
     Car carToFollow;
     ArrayList<Car> carGeneration = new ArrayList<>();
-    Image playerImage = new Image(carNumber);
     Image enemyImage = new Image("/sprites/car_yellow_3.png");
     int carCount = 50;
     Pane root;
+    public static String carColor = "";
     public static String carNumber = "";
     
     public AnimationTimer camera = new AnimationTimer() {
@@ -66,37 +66,7 @@ public class FXMLGameController {
 
     @FXML
     public void initialize() {
-        btnReset.setOnAction(resetEvent);
-    }
-
-    EventHandler<ActionEvent> resetEvent = new EventHandler<>() {
-        @Override
-        public void handle(ActionEvent event) {
-            removeAllCars();
-            spawner.spawn();
-            createCarGeneration();
-            road.resetLinePositions();
-        }
-    };
-
-    public void initalizeLevel() {
-        root = (Pane) Main.scene.getRoot();
-        createRoad();
-        spawner = new CarSpawner(4, -400, road, root, enemyImage);
-        createCarGeneration();
-        carController = new CarController(carGeneration, spawner.getCars());
-        Visualizer visualizer = new Visualizer(visualizerPane, carToFollow.getNeuralNetwork());
-        
-        camera.start();
-    }
-
-    public void createRoad() {
-        double roadWidth = roadPane.getPrefWidth() * 0.95;
-        road = new Road(roadPane.getPrefWidth() / 2, roadWidth);
-        root.getChildren().addAll(road.getLines());
-    }
-
-    public void createCarGeneration() {
+        Image playerImage = new Image("/sprites/car_"+carColor+"_"+carNumber+".png");
         for (int i = 0; i < carCount; i++) {
             Car newCar = new Car(road.getX_position_lane_two(), 450, playerImage);
             newCar.setRoad(road);
@@ -111,7 +81,35 @@ public class FXMLGameController {
         }
         carToFollow = carGeneration.get(0);
         carToFollow.setVisible(true);
+        btnReset.setOnAction(resetEvent);
     }
+
+    EventHandler<ActionEvent> resetEvent = new EventHandler<>() {
+        @Override
+        public void handle(ActionEvent event) {
+            removeAllCars();
+            spawner.spawn();
+            
+            road.resetLinePositions();
+        }
+    };
+
+    public void initalizeLevel() {
+        root = (Pane) Main.scene.getRoot();
+        createRoad();
+        spawner = new CarSpawner(4, -400, road, root, enemyImage);
+        carController = new CarController(carGeneration, spawner.getCars());
+        Visualizer visualizer = new Visualizer(visualizerPane, carToFollow.getNeuralNetwork());
+        
+        camera.start();
+    }
+
+    public void createRoad() {
+        double roadWidth = roadPane.getPrefWidth() * 0.95;
+        road = new Road(roadPane.getPrefWidth() / 2, roadWidth);
+        root.getChildren().addAll(road.getLines());
+    }
+
 
     private void chooseNextCar(double deathPosition) {
         Car nextCar = carToFollow;
